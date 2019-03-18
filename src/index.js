d4fd1985290d8676b6b2ffa10cbbd88747d48f48
Andrey Okhotnikov <an.okhotnikov@gmail.com>
2019-03-18 12:28:18 +0300

    create server

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { port } from "./utils/config";
+import app from "./app";
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
---
+++ src/utils/config.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import path from "path";
+
+const root = path.join.bind(this, __dirname, "../../");
+dotenv.config({ path: root(".env") });
+
+export const isProduction = process.env.NODE_ENV === "production";
+export const isDevelopment = !isProduction;
+
+export const url = process.env.URL;
+export const port = process.env.PORT;
+export const endpointURL = process.env.ENDPOINT_URL;
