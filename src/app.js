a3d058ce10cea1355f36d3639be14bf52e72f95b
Andrey Okhotnikov <an.okhotnikov@gmail.com>
2019-03-20 11:39:33 +0300

    update to apollo 2

--- src/app.js
+++ src/app.js
@@ -1,38 +1,40 @@
-import Koa from "koa";
-import Router from "koa-router";
-import koaBody from "koa-bodyparser";
-import cors from "@koa/cors";
-import helmet from "koa-helmet";
-import { makeExecutableSchema } from "graphql-tools";
-import { graphqlKoa, graphiqlKoa } from "apollo-server-koa";
+import Koa from 'koa';
+import koaBody from 'koa-bodyparser';
+import cors from '@koa/cors';
+import helmet from 'koa-helmet';
+import { ApolloServer } from 'apollo-server-koa';
 
-import typeDefs from "./schema.gql";
-import resolvers from "./resolvers";
-import { endpointURL, isDevelopment } from "./utils/config";
+import typeDefs from './schema.gql';
+import resolvers from './resolvers';
+import { endpointURL, isDevelopment } from './utils/config';
 
-const schema = makeExecutableSchema({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: {},
+  debug: isDevelopment
+});
 
 const app = new Koa();
-const router = new Router();
 
 app.use(helmet());
 app.use(koaBody());
 app.use(cors());
 
-router.all(
-  endpointURL,
-  // eslint-disable-next-line
-  graphqlKoa(ctx => ({
-    schema,
-    context: {},
-    debug: isDevelopment
-  }))
-);
+server.applyMiddleware({ app, path: endpointURL });
 
-if (isDevelopment) {
-  router.get("/graphiql", graphiqlKoa({ endpointURL }));
-}
+// router.all(
+//   endpointURL,
+//   // eslint-disable-next-line
+//   graphqlKoa(ctx => ({
+//     schema,
+//     context: {},
+//     debug: isDevelopment
+//   }))
+// );
 
-app.use(router.routes()).use(router.allowedMethods());
+// if (isDevelopment) {
+//   router.get('/graphiql', graphiqlKoa({ endpointURL }));
+// }
 
 export default app;
